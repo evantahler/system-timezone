385396d43a15eb3277383f3232dd5b962760488d
Evan Tahler <evantahler@gmail.com>
2015-09-19 09:45:30 -0700

    test for travis

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var should = require('should');
+var system_timezone = require('../index.js');
+
+describe('system tiemzone', function(){
+  it('should not crash', function(){
+    var timezone = system_timezone();
+    console.log('  timezone -> ' + timezone);
+  }); 
+});
